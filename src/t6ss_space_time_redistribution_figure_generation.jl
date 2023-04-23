8b75b1b4eb494af69120b2a08661ea75c104553a
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2023-04-23 01:01:39 +0100

    Added updated script

--- src/t6ss_space_time_redistribution_figure_generation.jl
+++ src/t6ss_space_time_redistribution_figure_generation.jl
@@ -7,9 +7,8 @@ using T6SSModellingStochasticTimerRW
 
 
 simulation_iterations = 4
-parameter_iterations = 50 
+parameter_iterations = 50
 input_values = pre_defined_params(simulation_iterations,parameter_iterations);
-
 
 figure_3 = generate_figure_3(input_values)
 save("figs/figure_3_a.png",figure_3[:a])
@@ -23,9 +22,10 @@ save("figs/figure_4_b.png",figure_4[:b])
 save("figs/figure_4_c.png",figure_4[:c])
 save("figs/figure_4_d.png",figure_4[:d])
 
-
 figure_5 = generate_figure_5(input_values)
 save("figs/figure_5_a.png",figure_5[:a])
 save("figs/figure_5_b.png",figure_5[:b])
 save("figs/figure_5_c.png",figure_5[:c])
+
+
 
